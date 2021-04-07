498c46bdbb3e81e5633774d2dfbcd8f333589906
apexad <1437332+apexad@users.noreply.github.com>
2021-04-06 20:32:45 -0700

    fix: revert to 0.4.1

--- index.js
+++ index.js
@@ -18,9 +18,6 @@ function GarageCmdAccessory(log, config) {
   this.pollStateDelay = config.poll_state_delay || 0;
   this.ignoreErrors = config.ignore_errors || false;
   this.logPolling = config.log_polling || false;
-  this.manufacturer = config.manufacturer || 'Apexad';
-  this.model = config.model || 'Garage Command';
-  this.serialNum = config.serialNum || '001';
 }
 
 GarageCmdAccessory.prototype.setState = function(isClosed, callback, context) {
@@ -74,53 +71,80 @@ GarageCmdAccessory.prototype.setState = function(isClosed, callback, context) {
   });
 };
 
+GarageCmdAccessory.prototype.getState = function(callback) {
+  var accessory = this;
+  var command = accessory.stateCommand;
+
+  exec(command, function (err, stdout, stderr) {
+    if (err) {
+      accessory.log('Error: ' + err);
+      callback(err || new Error('Error getting state of ' + accessory.name));
+    } else {
+      var state = stdout.toString('utf-8').trim();
+      if (state === 'STOPPED' && accessory.ignoreErrors) {
+        state = 'CLOSED';
+      }
+      if (accessory.logPolling) {
+        accessory.log('State of ' + accessory.name + ' is: ' + state);
+      }
+
+      callback(null, Characteristic.CurrentDoorState[state]);
+    }
+
+    if (accessory.pollStateDelay > 0) {
+      accessory.pollState();
+    }
+  });
+};
+
 GarageCmdAccessory.prototype.pollState = function() {
   var accessory = this;
 
+  // Clear any existing timer
   if (accessory.stateTimer) {
     clearTimeout(accessory.stateTimer);
     accessory.stateTimer = null;
   }
 
-  var command = accessory.stateCommand;
+  accessory.stateTimer = setTimeout(
+    function() {
+      accessory.getState(function(err, currentDeviceState) {
+        if (err) {
+          accessory.log(err);
+          return;
+        }
 
-  exec(command,
-    {
-      encoding: 'utf8',
-      timeout: 10000,
-      maxBuffer: 200*1024,
-      killSignal: 'SIGTERM',
-      cwd: null,
-      env: null
+        if (currentDeviceState === Characteristic.CurrentDoorState.OPEN || currentDeviceState === Characteristic.CurrentDoorState.CLOSED) {
+          // Set the target state to match the actual state
+          // If this isn't done the Home app will show the door in the wrong transitioning state (opening/closing)
+          accessory.garageDoorService.getCharacteristic(Characteristic.TargetDoorState)
+            .setValue(currentDeviceState, null, 'pollState');
+        }
+        accessory.garageDoorService.setCharacteristic(Characteristic.CurrentDoorState, currentDeviceState);
+      })
     },
-    function (err, stdout, stderr) {
-    var state = stdout.toString('utf-8').trim();
-    var doorState = Characteristic.CurrentDoorState[state] || Characteristic.CurrentDoorState.STOPPED;
-
-    if (doorState === Characteristic.CurrentDoorState.STOPPED && accessory.ignoreErrors) {
-      doorState = Characteristic.CurrentDoorState.CLOSED;
-    } else {
-      accessory.log('Error: ' + err);
-    }
-
-    accessory.garageDoorService.getCharacteristic(Characteristic.CurrentDoorState).setValue(doorState);
-    accessory.garageDoorService.getCharacteristic(Characteristic.TargetDoorState).setValue(doorState);
-  });
-
-  accessory.stateTimer = setTimeout(accessory.pollState(), accessory.pollStateDelay * 1000);
+    accessory.pollStateDelay * 1000
+  );
 }
 
 GarageCmdAccessory.prototype.getServices = function() {
-  this.informationService = new Service.AccessoryInformation()
-    .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
-    .setCharacteristic(Characteristic.Model, this.model)
-    .setCharacteristic(Characteristic.SerialNumber, this.serialNum);
+  this.informationService = new Service.AccessoryInformation();
   this.garageDoorService = new Service.GarageDoorOpener(this.name);
 
-  this.garageDoorService.getCharacteristic(Characteristic.TargetDoorState)
-    .on('set', this.setState.bind(this));
+  this.informationService
+  .setCharacteristic(Characteristic.Manufacturer, 'Garage Command')
+  .setCharacteristic(Characteristic.Model, 'Homebridge Plugin')
+  .setCharacteristic(Characteristic.SerialNumber, '001');
 
-  if (this.stateCommand) { this.pollState(); }
+  this.garageDoorService.getCharacteristic(Characteristic.TargetDoorState)
+  .on('set', this.setState.bind(this));
+
+  if (this.stateCommand) {
+    this.garageDoorService.getCharacteristic(Characteristic.CurrentDoorState)
+    .on('get', this.getState.bind(this));
+    this.garageDoorService.getCharacteristic(Characteristic.TargetDoorState)
+    .on('get', this.getState.bind(this));
+  }
 
   return [this.informationService, this.garageDoorService];
 };
